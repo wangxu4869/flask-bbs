992b301faa60883d68d6f975be705e16a16ff767
wangxu <653590766@qq.com>
2019-07-05 17:36:51 +0800

    sms timeout 120

--- utils/zlcache.py
+++ utils/zlcache.py
@@ -2,7 +2,7 @@ import memcache
 
 cache=memcache.Client(['127.0.0.1:11211'],debug=True)
 
-def set(key,vaule,timeout=60):
+def set(key,vaule,timeout=120):
     #封装的好处：以后再添加代码可以直接在此处添加一次即可，不用再视图函数中写cache连接参数
     return cache.set(key,vaule,timeout)
 

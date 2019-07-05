141980abf597325d7a146d1ee4fe4e264fc2afa1
wangxu <653590766@qq.com>
2019-07-05 17:17:53 +0800

    sms_captcha validate change 1111 to normal

--- apps/front/forms.py
+++ apps/front/forms.py
@@ -15,18 +15,18 @@ class SignupForm(BaseForm):
             sms_captcha=field.data
             telephone=self.telephone.data
 
-            if sms_captcha!='1111':
-                sms_captcha_mem=zlcache.get(telephone)
-                if not sms_captcha_mem or sms_captcha.lower() != sms_captcha_mem.lower():
-                    raise ValidationError(message='短信验证码错误！')
+            # if sms_captcha!='1111':
+            sms_captcha_mem=zlcache.get(telephone)
+            if not sms_captcha_mem or sms_captcha.lower() != sms_captcha_mem.lower():
+                raise ValidationError(message='短信验证码错误！')
 
     def validate_graph_captcha(self,field):
         graph_captcha=field.data
 
-        if graph_captcha!='1111':
-            graph_captcha_mem=zlcache.get(graph_captcha.lower())
-            if not graph_captcha_mem:
-                raise ValidationError(message='图形验证码错误！')
+        # if graph_captcha!='1111':
+        graph_captcha_mem=zlcache.get(graph_captcha.lower())
+        if not graph_captcha_mem:
+            raise ValidationError(message='图形验证码错误！')
 
 
 class SigninForm(BaseForm):
